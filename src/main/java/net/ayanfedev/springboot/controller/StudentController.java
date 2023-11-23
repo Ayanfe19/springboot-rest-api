3837156528bebd0ef98ae3a3ec4cfa6f20b533ac
Ayanfe <oluwafemi.wafi@gmail.com>
2023-11-24 00:41:34 +0100

    Return as bean

---
+++ src/main/java/net/ayanfedev/springboot/controller/StudentController.java
@@ -0,0 +1,21 @@
+package net.ayanfedev.springboot.controller;
+
+import net.ayanfedev.springboot.bean.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+
+    @GetMapping("/student")
+    public Student getStudent(){
+        Student student = new Student(
+                1,
+                "Victor",
+                "Ubuju"
+        );
+        return student;
+    }
+
+
+}
