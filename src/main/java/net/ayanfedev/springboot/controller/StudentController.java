1a71fce5d6cfb9066128b876d2600d1f4ee52cdf
Ayanfe <oluwafemi.wafi@gmail.com>
2023-11-24 01:07:10 +0100

    Using PathVariables

--- src/main/java/net/ayanfedev/springboot/controller/StudentController.java
+++ src/main/java/net/ayanfedev/springboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package net.ayanfedev.springboot.controller;
 
 import net.ayanfedev.springboot.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -29,4 +30,16 @@ public class StudentController {
         students.add(new Student(4, "Henry", "Phineas"));
         return students;
     }
+
+    // Spring Boot REST API with Path Variable
+    //    {id} - URI template variable
+    //     http://localhost:8080/students/1
+    @GetMapping("students/{id}/{first-name}/{last-name}")
+    public Student studentPathVariable(@PathVariable("id") int studentId,
+                                       @PathVariable("first-name") String firstName,
+                                       @PathVariable("last-name") String lastName){
+        return new Student(studentId, firstName, lastName);
+    }
+
+
 }
